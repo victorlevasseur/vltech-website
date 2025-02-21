b0e35477732ced8ba778324be57f5bd421f3d653
Victor Levasseur <victorlevasseur52@gmail.com>
2025-02-21 16:29:03 +0100

    feat: add CTA on the main page

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,7 @@ export const CONFIG = {
   domain: process.env.DOMAIN!,
 
   gtmId: process.env.GTM_ID && process.env.GTM_ID !== 'none' ? process.env.GTM_ID : undefined,
+
+  maltUrl: process.env.MALT_URL ?? '#',
+  calendlyUrl: process.env.CALENDLY_URL ?? '#',
 }
