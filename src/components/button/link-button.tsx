fb2aaf671ac8ea55f55fda53d3ccaa2dc3597e52
Victor Levasseur <victorlevasseur52@gmail.com>
2025-02-21 16:28:43 +0100

    feat: add LinkButton

---
+++ src/components/button/link-button.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import styles from "./link-button.module.scss";
+import {HTMLAttributeAnchorTarget} from "react";
+
+export interface LinkButtonProps {
+  label: string;
+  href: string;
+  target?: HTMLAttributeAnchorTarget;
+  variant?: "primary" | "secondary";
+}
+
+export const LinkButton: React.FC<LinkButtonProps> = (props) => {
+  const variant = props.variant || "primary";
+  return (
+    <Link
+      className={styles.container + ' ' + styles[variant]}
+      href={props.href}
+      target={props.target}>
+      {props.label}
+    </Link>
+  )
+}
