6c8d63cda5ad1909a31c1ad66519f7a9a0907acc
Victor Levasseur <victorlevasseur52@gmail.com>
2025-02-25 16:47:05 +0100

    test: add link-button component test

--- src/components/button/link-button.stories.tsx
+++ src/components/button/link-button.stories.tsx
@@ -1,8 +1,14 @@
 import {Meta, StoryObj} from "@storybook/react";
 import {LinkButton} from "@/components/button/link-button";
+import {expect, within} from "@storybook/test";
 
 const meta: Meta<typeof LinkButton> = {
   component: LinkButton,
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    const link = await canvas.findByText('Foobar');
+    await expect(link.getAttribute('href')).toEqual('https://www.foobar.com/');
+  }
 }
 
 export default meta;
