455ae27d9cb4075362ad555514b874c5406a21ca
Victor Levasseur <victorlevasseur52@gmail.com>
2025-02-26 22:06:10 +0100

    feat: implement mobile navigation modal

--- src/components/menu-bar/menu-bar-link.tsx
+++ src/components/menu-bar/menu-bar-link.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import styles from './menu-bar-link.module.scss';
 import Link from 'next/link';
-import {hasProperty} from "@vitest/expect";
 
-export type MenuBarLinkProps = {
+export interface MenuBarLinkProps {
   label: string;
   active?: boolean;
   href: string;
+  onClick?: () => void;
 }
-
 
 const MenuBarLink: React.FC<MenuBarLinkProps> = (props) => {
   return (
     <Link
       className={`${styles.container} ${props.active ? styles.active : ''}`}
       href={props.href}
+      onClick={props.onClick}
       data-testid={'link'}
     >
       {props.label}
