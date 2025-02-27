f8059061e4ffffb1c350ffefb6172ef2c1f40679
Victor Levasseur <victorlevasseur52@gmail.com>
2025-02-27 11:04:32 +0100

    test: Storybook test runner now uses the default viewport of each story if possible

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,12 +1,33 @@
-import type { TestRunnerConfig } from '@storybook/test-runner';
-import { injectAxe, checkA11y } from 'axe-playwright';
+import {getStoryContext, TestRunnerConfig} from '@storybook/test-runner';
+import {checkA11y, injectAxe} from 'axe-playwright';
+import {VIEWPORTS} from "./viewports";
 
-/*
- * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
- * to learn more about the test-runner hooks API.
- */
+const DEFAULT_VIEWPORT_SIZE = { width: 1200, height: 900 };
+
 const config: TestRunnerConfig = {
-  async preVisit(page) {
+  async preVisit(page, story) {
+    const context = await getStoryContext(page, story);
+
+    // Set the viewport to the default viewport of the story
+    // to best mimic Storybook behavior.
+    const viewportName = context.parameters?.viewport?.defaultViewport;
+    const viewportParameter = VIEWPORTS[viewportName];
+
+    if (viewportParameter) {
+      const viewportSize = Object.entries(viewportParameter.styles ?? {}).reduce(
+        (acc, [screen, size]) => ({
+          ...acc,
+          // make sure your viewport config in Storybook only uses numbers, not percentages
+          [screen]: parseInt(size),
+        }),
+        DEFAULT_VIEWPORT_SIZE
+      );
+
+      await page.setViewportSize(viewportSize);
+    } else {
+      await page.setViewportSize(DEFAULT_VIEWPORT_SIZE);
+    }
+
     await injectAxe(page);
   },
   async postVisit(page) {
