54bac0fce97d04019423853f15e13ede41feae1d
Victor Levasseur <victorlevasseur52@gmail.com>
2025-03-02 22:56:08 +0100

    feat: add info card component

---
+++ src/components/card/info-card.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from './info-card.module.scss';
+import {LinkButton} from "@/components/button/link-button";
+
+export interface InfoCardLink {
+  href: string;
+  label: string;
+}
+
+export interface InfoCardProps {
+  title: string;
+  children?: React.ReactNode;
+
+  className?: string;
+
+  link?: InfoCardLink;
+}
+
+export const InfoCard: React.FC<InfoCardProps> = (props) => {
+  return (
+    <div className={`${styles.container} ${props.className}`}>
+      <h2>{props.title}</h2>
+      {props.children}
+      {
+        props.link ?
+          (<div className={styles.linkContainer}>
+            <LinkButton label={props.link.label} href={props.link.href} variant={'secondary'} />
+          </div>) :
+          null
+      }
+    </div>
+  );
+}
